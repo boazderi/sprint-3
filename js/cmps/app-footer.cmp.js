4accd63ac6373d2af1e7a37481714d3021a79f4b
Boaz Deri <boazderi@gmail.com>
2022-11-08 23:48:10 +0200

    add to footer yair section

--- js/cmps/app-footer.cmp.js
+++ js/cmps/app-footer.cmp.js
@@ -4,13 +4,42 @@ export default {
     name: 'footer-cmp',
     template: `
     <footer class="app-footer flex align-items-center justify-center">
-        <img src="./img/my-photo.jpeg" width=80 class="my-photo" alt="">
-        
-        <div class=" flex flex-column justify-center align-items-center">
+        <section class="yair-section">
+            <img src="./img/yair-photo.jpeg" width=80 class="my-photo" alt="">
+            <div class=" flex flex-column justify-center align-items-center">
+                <div class="flex icons-container justify-evenly align-items-center">
+                    <a href="https://www.linkedin.com/in/yair-orchen-11572a223" target="_blank">
+                        <div class="icon">
+                            <img class="linkedin-icon img-icon" src="./img/linkedin.png" width="30" height="30" alt="">
+                        </div>
+                    </a>
+                    <a href="https://www.facebook.com/yair.orchen" target="_blank">
+                        <div class="icon">
+                            <img class="facebook-icon img-icon" src="./img/facebook.png" width="30" height="30" alt="">
+                        </div>
+                    </a>
+                    <a href="https://twitter.com/boaz_deri" target="_blank">
+                        <div class="icon">
+                            <img class="twitter-icon img-icon" src="./img/twitter.png" width="30" height="30" alt="">
+                        </div>
+                    </a>
+                    <a href="https://www.instagram.com/yairorchen/" target="_blank">
+                        <div class="icon">
+                            <img class="instagram-icon img-icon " src="./img/instagram.png" width="30" height="30" alt="">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+        <div class="p-section">
             <small class="p-container">
                 &#9400; This site developed by Boaz Deri, find me here
             </small>
-            
+        </div>
+        <section class="boaz-section">
+        <img src="./img/my-photo.jpeg" width=80 class="my-photo" alt="">
+        
+        <div class=" flex flex-column justify-center align-items-center">
             <div class="flex icons-container justify-evenly align-items-center">
                 <a href="https://www.linkedin.com/in/boaz-deri/" target="_blank">
                     <div class="icon">
@@ -34,9 +63,10 @@ export default {
                 </a>
             </div>
         </div>
+        </section>
     </footer>
     `,
-    created(){
+    created() {
         eventBus.on('test-event', payload => console.log(payload))
     }
 }
