17fe9748b9bfed3133e3ceaa5c7ccf1e8d19bde4
Yair Orchen <yairorchen1@gmail.com>
2022-11-08 23:21:30 +0200

    fix something

--- js/routes.js
+++ js/routes.js
@@ -1,41 +1,34 @@
-
-import bookApp from "./view/book-app-cmp.js"
-import homePage from "./view/home-page.cmp.js"
-import aboutPage from "./view/about-page.cmp.js"
-import bookDetails from "./view/book-details-cmp.js"
-import bookAdd from "./cmps/book-add.cmp.js"
+import bookApp from './view/book-app-cmp.js'
+import homePage from './view/home-page.cmp.js'
+import aboutPage from './view/about-page.cmp.js'
+import bookDetails from './view/book-details-cmp.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
 const routerOptions = {
-    history: createWebHashHistory(),
-    routes: [
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: homePage,
+    },
+    {
+      path: '/book',
+      component: bookApp,
+      children: [
         {
-            path: '/',
-            component: homePage
+          path: '/book/:id',
+          component: bookDetails,
+          name: 'details',
+          props: true,
         },
-        {
-            path: '/book',
-            component: bookApp,
-            children:[
-
-                {
-                    path: '/book/:id',
-                    component: bookDetails,
-                    name:'details',
-                    props:true
-                }
-            ]
-        },
-        {
-            path: '/book/add',
-            component: bookAdd
-        },
-        {
-            path: '/about',
-            component: aboutPage
-        },
-    ]
+      ],
+    },
+    {
+      path: '/about',
+      component: aboutPage,
+    },
+  ],
 }
 
 export const router = createRouter(routerOptions)
